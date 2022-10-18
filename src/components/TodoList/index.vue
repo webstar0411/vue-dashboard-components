6422f1377f3c916e98ef84b366d8e7231c680651
webstar0411 <webstar0411@gmail.com>
2022-10-19 01:19:37 +0900

    update

--- src/components/TodoList/index.vue
+++ src/components/TodoList/index.vue
@@ -4,23 +4,30 @@
       <span class="title">Incidencias</span>
       <span class="svg-button">
         <div class="todo-tooltip">
+          <img v-if="taskIdx === 0" :src="SuccessSVG" class="success" />
           <img
             :src="FileSVG"
-            @click="() => $emit('onChange', 'tasks', 'tasks')"
+            @click="() => ($emit('onChange', 'tasks', 'tasks'), (taskIdx = 0))"
           />
           <span class="tooltiptext">Task</span>
         </div>
         <div class="todo-tooltip">
+          <img v-if="taskIdx === 1" :src="SuccessSVG" class="success" />
           <img
             :src="FileOutlineSVG"
-            @click="() => $emit('onChange', 'tasks', 'recommandation')"
+            @click="
+              () => (
+                $emit('onChange', 'tasks', 'recommandation'), (taskIdx = 1)
+              )
+            "
           />
           <span class="tooltiptext">Recommendation</span>
         </div>
         <div class="todo-tooltip">
+          <img v-if="taskIdx === 2" :src="SuccessSVG" class="success" />
           <img
             :src="BagSVG"
-            @click="() => $emit('onChange', 'tasks', 'issue')"
+            @click="() => ($emit('onChange', 'tasks', 'issue'), (taskIdx = 2))"
           />
           <span class="tooltiptext">Issue</span>
         </div>
@@ -38,11 +45,16 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import TodoItem from "./TodoItem.vue";
 import FileSVG from "./svg/file.svg";
 import FileOutlineSVG from "./svg/fileOutline.svg";
 import BagSVG from "./svg/bag.svg";
+import SuccessSVG from "./svg/success.svg";
+
 import "./style.scss";
+
+const taskIdx = ref(0);
 
 const { data, company } = defineProps(["data", "company"]);
 </script>
